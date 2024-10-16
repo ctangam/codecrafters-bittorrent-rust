c428a0a6639f98ed5f8dfa7e7e93e21682835e68
ctangam <luochenjoey@gmail.com>
2024-10-16 17:45:22 +0800

    add download

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,12 @@ enum Command {
         torrent: PathBuf,
         piece_id: usize,
     },
+
+    Download {
+        #[arg(short)]
+        output: PathBuf,
+        torrent: PathBuf,
+    },
 }
 
 #[allow(dead_code)]
@@ -247,7 +253,6 @@ async fn main() -> anyhow::Result<()> {
             })
             .await
             .context("send interested message")?;
-        
 
             let unchoke = peer
                 .next()
@@ -317,6 +322,139 @@ async fn main() -> anyhow::Result<()> {
                 .await
                 .context("write out downloaded piece")?;
             println!("Piece {piece_id} downloaded to {}.", output.display());
+        }
+        Command::Download { output, torrent } => {
+            let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
+            let tor: Torrent =
+                serde_bencode::from_bytes(&dot_torrent).context("parse torrent file")?;
+            let length = if let torrent::Keys::Single { length } = tor.info.keys {
+                length
+            } else {
+                todo!();
+            };
+            let info_hash = tor.info_hash();
+
+            let params = TrackerRequest {
+                peer_id: "00112233445566778899".into(),
+                port: 6881,
+                uploaded: 0,
+                downloaded: 0,
+                left: length,
+                compact: 1,
+            };
+            let encoded = serde_urlencoded::to_string(params).context("url encode params")?;
+            let query_url = format!(
+                "{}?{}&info_hash={}",
+                tor.announce,
+                encoded,
+                urlencode(&info_hash)
+            );
+            let res = reqwest::get(query_url).await.context("query tracker")?;
+            let res = res.bytes().await.context("fetch tracker res")?;
+            let tracker_info: TrackerResponse =
+                serde_bencode::from_bytes(&res).context("parse tracker res")?;
+
+            let peer = tracker_info.peers.0[0];
+            let mut peer = TcpStream::connect(peer).await.context("connect peer")?;
+
+            let mut handshake = Handshake::new(info_hash, *b"00112233445566778899");
+            {
+                let handshake_bytes = handshake.as_bytes_mut();
+                peer.write_all(handshake_bytes)
+                    .await
+                    .context("send handshake")?;
+                peer.read_exact(handshake_bytes)
+                    .await
+                    .context("read handshake")?;
+            }
+
+            let mut peer = tokio_util::codec::Framed::new(peer, MessageFramer);
+            let bitfield = peer
+                .next()
+                .await
+                .expect("bitfield msg")
+                .context("read bitfield")?;
+
+            peer.send(Message {
+                tag: MessageTag::Interested,
+                payload: Vec::new(),
+            })
+            .await
+            .context("send interested message")?;
+
+            let unchoke = peer
+                .next()
+                .await
+                .expect("unchoke msg")
+                .context("read unchoke")?;
+            assert_eq!(unchoke.tag, MessageTag::Unchoke);
+            assert!(unchoke.payload.is_empty());
+
+            let mut all_pieces = Vec::with_capacity(length);
+            for piece_id in 0..tor.info.pieces.0.len() {
+                let piece_size = if piece_id == tor.info.pieces.0.len() - 1 {
+                    let md = length % tor.info.plength;
+                    if md == 0 {
+                        tor.info.plength
+                    } else {
+                        md
+                    }
+                } else {
+                    tor.info.plength
+                };
+
+                let nblocks = (piece_size + BLOCK_MAX - 1) / BLOCK_MAX;
+                let mut all_blocks = Vec::with_capacity(piece_size);
+                for block in 0..nblocks {
+                    let block_size = if block == nblocks - 1 {
+                        let md = piece_size % BLOCK_MAX;
+                        if md == 0 {
+                            BLOCK_MAX
+                        } else {
+                            md
+                        }
+                    } else {
+                        BLOCK_MAX
+                    };
+
+                    let mut request = Request::new(
+                        piece_id as u32,
+                        (block * BLOCK_MAX) as u32,
+                        block_size as u32,
+                    );
+                    peer.send(Message {
+                        tag: MessageTag::Request,
+                        payload: request.as_bytes_mut().to_vec(),
+                    })
+                    .await
+                    .with_context(|| format!("send request for block {block}"))?;
+
+                    let piece = peer
+                        .next()
+                        .await
+                        .expect("receive piece msg")
+                        .context("invalid piece msg")?;
+                    let piece = Piece::ref_from_bytes(&piece.payload)
+                        .expect("always get all Piece response fields from peer");
+                    all_blocks.extend(piece.block())
+                }
+
+                let piece_hash = &tor.info.pieces.0[piece_id];
+                let mut hasher = Sha1::new();
+                hasher.update(&all_blocks);
+                let hash: [u8; 20] = hasher
+                    .finalize()
+                    .try_into()
+                    .expect("GenericArray<_, 20> == [_; 20]");
+                assert_eq!(&hash, piece_hash);
+
+                all_pieces.extend(&all_blocks);
+                println!("Piece {piece_id} downloaded.");
+            }
+            tokio::fs::write(&output, all_pieces)
+                .await
+                .context("write out downloaded piece")?;
+            println!("Downloaded to {}.", output.display());
         }
     }
 
